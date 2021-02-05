dbe598482644300e1f59eed64ab2103f146f86bb
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-05 15:18:35 -0500

    Made an get route

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,14 @@ const Workout = require("../models/Workout");
 // const { model } = require("../models/workout");
 const Router = express.Router();
 
+Router.get("/api/workouts", (req, res) => {
+    Workout.find({}).then((workout) => {
+        console.log(workout);
+        res.json(workout);
+    });
+});
+
+
 
 
 module.exports = Router;
