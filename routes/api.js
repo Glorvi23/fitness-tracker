1ff87a21e95ba50db6116fe1544c016b197c6aa3
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-05 15:18:05 -0500

    Set up an api for routes

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const Workout = require("../models/Workout");
+// const { model } = require("../models/workout");
+const Router = express.Router();
+
+
+
+module.exports = Router;
