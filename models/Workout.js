cb8c2b69037b3fdff84d66570e22cc1487875c90
Caleb Glorvigen <jcglorvi@gmail.com>
2021-02-08 23:26:52 -0500

    Created the model

--- models/Workout.js
+++ models/Workout.js
@@ -1,37 +1,77 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
+// const Schema = mongoose.Schema;
+
+// const WorkoutSchema = new Schema({
+//     day: {
+//         type: Date,
+//         default: () => new Date() 
+//     },
+//     exercises: [{
+//         type: {
+//             type: String,
+//         },
+//         name: {
+//             type: String
+//         },
+//         duration: {
+//             type: Number
+//         },
+//         weight: {
+//             type: Number
+//         },
+//         reps: {
+//             type: Number
+//         },
+//         sets: {
+//             type: Number
+//         },
+//         distance: {
+//             type: Number
+//         }
+//     }]
+// });
+
+// const Workout = mongoose.model("Workout", WorkoutSchema);
+
+// module.exports = Workout;
+
+
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema({
+const workoutSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
+        default: () => new Date(),
     },
     exercises: [{
         type: {
             type: String,
+            trim: true,
+            required: 'Enter an exercise type',
         },
         name: {
-            type: String
+            type: String,
+            trim: true,
+            required: 'Enter an exercise name',
         },
         duration: {
-            type: Number
+            type: Number,
+            required: 'Enter an exercise duration in minutes',
         },
         weight: {
-            type: Number
+            type: Number,
         },
         reps: {
-            type: Number
+            type: Number,
         },
         sets: {
-            type: Number
+            type: Number,
         },
         distance: {
-            type: Number
-        }
-    }]
+            type: Number,
+        },
+    }, ],
 });
-
-const Workout = mongoose.model("Workout", WorkoutSchema);
-
+const Workout = mongoose.model('Workout', workoutSchema);
 module.exports = Workout;
